5901a118a7f36bd473e0666cae3180e52bcd8d15
Ihustat <ger26dan@gmail.com>
2023-12-12 10:44:27 +0300

    add nav functional

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,26 @@
-'use strict'
+'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+  ///nav
 
+  const navList = document.querySelector('.nav__list'),
+    navItems = document.querySelectorAll('.nav__list-item');
 
+  navList.addEventListener('click', (e) => {
+    const target = e.target;
 
+    if (target && target.classList.contains('nav__list-item')) {
+      navItems.forEach((item) => {
+        target === item
+          ? item.classList.add('nav__list-item_active')
+          : item.classList.remove('nav__list-item_active');
+      });
+    }
+
+    if (target && target.classList.contains('nav-cross')) {
+      navItems.forEach((item) => {
+        item.classList.remove('nav__list-item_active');
+      });
+    }
+  });
 });
